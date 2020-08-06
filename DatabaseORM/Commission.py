1547ef0774ee44f956ff29f8d31a103ca6bf4ff0
s <smos@list.ru>
2020-08-06 15:12:39 +0300

    Changes in table declaration

--- DatabaseORM/Commission.py
+++ DatabaseORM/Commission.py
@@ -1,4 +1,4 @@
-from sqlalchemy import String, Column, Integer, Enum, ForeignKey
+from sqlalchemy import String, Column, Integer, Enum, ForeignKey, BigInteger, Date
 
 from DatabaseORM import Base
 from DatabaseORM.enums import CommissionType
@@ -8,13 +8,21 @@ class Commission(Base):
 
     __tablename__ = 'commission'
     id = Column(Integer, primary_key=True)
+    iz_id = Column(BigInteger)
 
     name = Column(String)
     commission_type = Column(Enum(CommissionType))
     address = Column(String)
     superior_commission_id = Column(Integer, ForeignKey('commission.id'))
-    district_id = Column(Integer, ForeignKey('district.id'))
-    election_id = Column(Integer, ForeignKey('election.id'))
     region_id = Column(Integer, ForeignKey('region.id'))
-    longitude = Column(String)
-    latitude = Column(String)
+    phone = Column(String)
+    fax = Column(String)
+    email = Column(String)
+    end_date = Column(String)
+    address_voteroom = Column(String)
+    phone_voteroom = Column(String)
+    lat_ik = Column(String)
+    lon_ik = Column(String)
+    lat_voteroom = Column(String)
+    lon_voteroom = Column(String)
+    snapshot_date = Column(Date)
--- DatabaseORM/__init__.py
+++ DatabaseORM/__init__.py
@@ -6,11 +6,8 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import psycopg2
 
-
-
 with open('credentials.json') as credentials:
     credentials = json.load(credentials)
-
     url = sqlalchemy.engine.url.URL(drivername=credentials['drivername'],
                                     username=credentials['username'],
                                     password=credentials['password'],
@@ -18,7 +15,7 @@ with open('credentials.json') as credentials:
                                     port=credentials['port'],
                                     database=credentials['database'],
                                     query=None)
+
 engine = create_engine(url)
 Session = sessionmaker(bind=engine)
-
 Base = declarative_base()
