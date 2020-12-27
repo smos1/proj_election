c80c9f5c3f6e5c44755b0914a430b99aeb164f6d
smos1 <10451777+smos1@users.noreply.github.com>
2020-12-27 22:44:42 +0300

    Parser refactoring

--- parser_preliminary_results/helper.py
+++ parser_preliminary_results/helper.py
@@ -1,4 +1,3 @@
-# In[1]:
 from bs4 import BeautifulSoup
 import pandas as pd
 import os
@@ -11,11 +10,9 @@ from io import BytesIO
 from selenium.common.exceptions import NoSuchElementException
 from selenium.webdriver.support.ui import Select
 import re
+import numpy as np
 
-os.chdir(r'D:\Documents\GitHub\proj_election\parser_preliminary_results')
-
-# In[2]:
-pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract.exe'
+SLEEP_TIME = 0.5
 
 def detect_captcha_text(image: BinaryIO) -> str:
     """Detects captcha text
@@ -24,44 +21,20 @@ def detect_captcha_text(image: BinaryIO) -> str:
     Returns:
         string: captcha symbols
     """
-    
     stream = BytesIO(image)
     image_rgb = Image.open(stream).convert("RGBA")
     stream.close()
     gray_image = image_rgb.convert("L")
-    th1 = 130
-    number_of_files=len(os.listdir('./captchas/'))/3
-    gray_image.save(f"./captchas/{number_of_files+1}_first.png")
 
-    first_threshold = gray_image.point(lambda p: p > th1 and 255)
-    first_threshold.save(f"./captchas/{number_of_files+1}_first_threshold.png")
-    final = first_threshold.filter(ImageFilter.EDGE_ENHANCE_MORE)
-    final = final.filter(ImageFilter.SHARPEN)
-    final.save(f"./captchas/{number_of_files+1}_final.png")
-
-    text_image = pytesseract.image_to_string(final,
-                                             config='--psm 4 --oem 1 -c tessedit_char_whitelist=0123456789')
+    text_image = pytesseract.image_to_string(gray_image,
+                                             config='--psm 7 -c tessedit_char_whitelist=0123456789')
     text_image=re.sub('[\W]', '', text_image)
-    print(text_image.strip())
+
     if len(text_image) !=5:
         text_image = '00000'
-    # remove non-alpha numeric text
+
     return text_image
 
-
-def replace_o_as_0(list_of_char: List) -> List:
-    """Replaces O's with zeroes
-    Args:
-        list_of_char (List): list of chars
-    Returns:
-        List: list of chars with replaced symbols
-    """
-    for index in range(len(list_of_char)):
-        if list_of_char[index] == "o":
-            list_of_char[index] = "0"
-        else:
-            continue
-    return list_of_char
 
 
 def find_and_recognize_captcha(driver) -> Dict:
@@ -72,15 +45,12 @@ def find_and_recognize_captcha(driver) -> Dict:
         Dict: captcha input field and captcha text
     """
     captcha_png = driver.find_element_by_id("captchaImg").screenshot_as_png
-    captcha_text = detect_captcha_text(captcha_png)
-    text_list = list(captcha_text)
-    text_list_prep = replace_o_as_0(text_list)
-    text_prep = "".join(text_list_prep)
+    captcha_text = detect_captcha_text(captcha_png).replace("o", "0")
     captcha_input_field = driver.find_element_by_id("captcha")
-    return {"captcha_input_field": captcha_input_field, "captcha_text": text_prep}
+    return {"captcha_input_field": captcha_input_field, "captcha_text": captcha_text}
 
 
-def get_through_captcha(driver, url: str) -> None:
+def get_through_captcha(driver, url: str):
     """Iterativly tries to get through captcha
     Args:
         driver: selenium driver
@@ -90,52 +60,57 @@ def get_through_captcha(driver, url: str) -> None:
     """
 
     driver.get(url)
-    time.sleep(1)
+    time.sleep(SLEEP_TIME)
     flag = 1
     while flag:
         try:
-            #driver.refresh()
-            print('boo')
             element_dlya = driver.find_element_by_xpath("//table")
             flag = 0
-            print('True')
         except NoSuchElementException:
-            empty_space = driver.find_element_by_xpath("//html")
-            empty_space.click()
+            #empty_space = driver.find_element_by_xpath("//html")
+            #empty_space.click()
             captcha_elements = find_and_recognize_captcha(driver)
             captcha_input_field = captcha_elements["captcha_input_field"]
             captcha_text = captcha_elements["captcha_text"]
             captcha_input_field.send_keys(captcha_text)
             driver.find_element_by_id("send").click()
-            time.sleep(2)
-            driver.refresh()
-    return None
-
-# In[3]:
+            time.sleep(SLEEP_TIME)
 
 
-def get_election_result(url: str, driver) -> pd.DataFrame:
+
+def get_election_result(url: str, driver, level=1) -> pd.DataFrame:
     """Load election result data
     Args:
         url (str): url link
         driver : selenium driver
+        level: 1/2 position of link to election table from the bottom of the page (1 for UIK data, 2 for summary data)
     Returns:
         None: returns nothing, it downloads data
     """
     driver.get(url)
 
-    time.sleep(1)
+    time.sleep(SLEEP_TIME)
     get_through_captcha(driver, url)
-    vote_table = driver.find_elements_by_css_selector("tr>td>nobr>a")[-1]
+    vote_table = driver.find_elements_by_css_selector("tr>td>nobr>a")[-level]
     vote_table.click()
-    #get_through_captcha(driver, url)
-    table = pd.read_html(
-        str(BeautifulSoup(driver.page_source, features="lxml").select('table:nth-of-type(5)')))[0]
-    return table
+    table_result = str(BeautifulSoup(driver.page_source, features="lxml").select('table:nth-of-type(5)'))
+    if table_result=='[]':
+        return None
+    else:
+        return pd.read_html(table_result)[0]
 
 
-def test_if_new_layers(driver, level_name: str, inceptions_level=0, output=dict()) -> list:
+
+def test_if_new_layers(driver, elections_url, level_name=None, inceptions_level=0, output=[], summary_found="", summary_level_name=None, path=[]) -> list:
     flag_direct_selection = 0
+
+    if not summary_found:
+        cur_url = driver.current_url
+        table = get_election_result(driver.current_url, driver, level=2)
+        if type(table)==pd.DataFrame:
+            summary_found = cur_url
+            summary_level_name = level_name
+        driver.back()
 
     try:
         link_to_real_data = driver.find_element_by_xpath(
@@ -152,33 +127,39 @@ def test_if_new_layers(driver, level_name: str, inceptions_level=0, output=dict(
             "/html/body/table[2]/tbody/tr[2]/td/a")
         link_to_real_data.click()
 
-        #print([k['value'] for k in BeautifulSoup(driver.page_source).select('option[value]')])
-        if inceptions_level == 0:
-            output.update({'direct': [k['value'] for k in BeautifulSoup(
-                driver.page_source, features="lxml").select('option[value]')]})
-        else:
-            output.update({level_name: [k['value'] for k in BeautifulSoup(
-                driver.page_source, features="lxml").select('option[value]')]})
+        path = 'direct' if inceptions_level==0 else path
+        output += [{'uik':k.next,
+                    'path':path,
+                    'summary_found':summary_found,
+                    'summary_level_name':summary_level_name,
+                    'link_to_UIK':k['value'],
+                    'elections_url':elections_url} for k in BeautifulSoup(
+                driver.page_source, features="lxml").select('option[value]')]
+                           
         return output
 
     elif flag_direct_selection == 2:
         # if we have a dropdown menu with OIK
-        options = BeautifulSoup(driver.page_source).select('option[value]')
+        options = BeautifulSoup(driver.page_source, features="lxml").select('option[value]')
         number_of_OIK = len(options)
         options_values = [i.text for i in options]
 
         # to know how many times we need to walk around
-        print('total', number_of_OIK)
-        for i in range(2, number_of_OIK+2):
+        for i in range(number_of_OIK):
             form = driver.find_element_by_xpath(
                 "/html/body/table[2]/tbody/tr[2]/td/form/select")
-            Select(form).select_by_visible_text(f'{options_values[i-2]}')
+            Select(form).select_by_visible_text(f'{options_values[i]}')
             driver.find_element_by_xpath(
                 "/html/body/table[2]/tbody/tr[2]/td/form/input").click()
             inceptions_level += 1
-            output.update({options_values[i-2]: test_if_new_layers(
-                driver, options_values[i-2], inceptions_level, dict())})
-
+            test_if_new_layers(driver=driver,
+                               elections_url=elections_url,
+                               level_name=options_values[i],
+                               inceptions_level=inceptions_level,
+                               output = output,
+                               summary_found=summary_found,
+                               summary_level_name=summary_level_name,
+                               path=path+[options_values[i]])
             back_steps(driver)
 
         return output
@@ -197,20 +178,17 @@ def back_steps(driver) -> int:
         return None
 
 
-def get_links_UIK(link: str, dct: dict, driver="driver") -> list:
-    driver.get(link)
-    time.sleep(1)
+def get_links_UIK(link: str, dct: dict, driver) -> list:
+    #time.sleep()
     get_through_captcha(driver, link)
-    list_of_links_to_UIK_data = dict()
 
-    list_of_links_to_UIK_data = test_if_new_layers(driver, 'direct', 0, dct)
+    list_of_links_to_UIK_data = test_if_new_layers(driver=driver,
+                                                   elections_url=link,
+                                                   level_name='direct',
+                                                   inceptions_level=0,
+                                                   path=[],
+                                                   output=[])
 
     return list_of_links_to_UIK_data
 
 
-
-# get data
-def runner(urls: dict) -> dict:
-    driver_path = "D:\Downloads_new\chromedriver_win32/chromedriver.exe"
-    driver = webdriver.Chrome(driver_path)
-    return {i: get_election_result(j, driver) for i,j in urls.items()}
